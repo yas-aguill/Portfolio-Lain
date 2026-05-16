606d6000a610532dc599e3a299346a41b71ff8a2
yas-aguill <yasmin.aguilleraa@gmail.com>
2026-05-16 11:22:31 -0300

    modified:   index.html

---
+++ lain/script.js
@@ -0,0 +1,20 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".menu a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 120;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
